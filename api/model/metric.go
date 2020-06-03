16ff5be5c07c26a0d7897559f1aabc90267a9a5a
James Hogle <jhogle117@gmail.com>
2020-06-02 19:38:29 -0500

    removing comments

--- api/model/metric.go
+++ api/model/metric.go
@@ -1,12 +1,3 @@
-// id UUID not null,
-// gauge_id UUID not null,
-// gauge_type text not null,
-// value double precision not null,
-// recorded_date date not null,
-// primary key (id),
-// constraint metric_gauge_fk foreign key (gauge_id)
-// 	references gauge (id)
-
 package model
 
 import (
